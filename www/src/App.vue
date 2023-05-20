ac046a92535440750cdb1006cde3ae7c85a7a546
Rémi Connesson <remiconnesson2@gmail.com>
2023-05-21 00:23:10 +0200

    It works

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
-import { greet } from "game-of-life-rs";
+import { Universe } from "game-of-life-rs";
+import { ref } from 'vue'
 
-const hello = "world";
+const universe = Universe.new()
+const gameOfLife = ref('loading life...')
+
+gameOfLife.value = universe.render()
+
+function tick () {
+  universe.tick()
+  gameOfLife.value = universe.render()
+}
+
+setInterval(tick, 750)
 </script>
 
 <template>
-  <h1>{{ hello }}</h1>
-  <button @click="greet()">Greet</button>
+  <div id="game-of-life">
+    <pre>{{ gameOfLife }}</pre>
+  </div>
 </template>
+
+<style scoped>
+ #game-of-life{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%; 
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 4rem;
+}
+
+pre {
+  font-size: 8px;
+}
+</style>
+
