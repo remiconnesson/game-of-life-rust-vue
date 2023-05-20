cba3a722b7a1c4f6d9310e2bdef0494ae250da47
Rémi Connesson <remiconnesson2@gmail.com>
2023-05-20 22:43:15 +0200

    Make it work with vue

---
+++ www/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { greet } from "game-of-life-rs";
+
+const hello = "world";
+</script>
+
+<template>
+  <h1>{{ hello }}</h1>
+  <button @click="greet()">Greet</button>
+</template>
